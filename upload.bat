de0849d98b72c40199fc18820f32a288e0f5e0dc
ahmadimran962527 <ahmadimran962527@gmail.com>
2026-05-16 09:20:27 -0700

    updated code

---
+++ upload.bat
@@ -0,0 +1,12 @@
+@echo off
+cd "C:\Users\Ahmad\Desktop\Ahmad Ai Assistant"
+echo.
+echo Uploading to GitHub...
+echo.
+git add .
+git commit -m "updated code"
+git push
+echo.
+echo Done! Code uploaded to GitHub successfully!
+echo.
+pause
